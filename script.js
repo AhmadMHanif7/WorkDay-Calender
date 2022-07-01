a5640ad7ebe7c5e5e7f52b8fdd182c53eb2a372c
Ahmad Hanif <ahmadmhanif7@gmail.com>
2022-06-30 20:59:04 -0500

    Final style changes and initial local storage options

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.getElementById("currentDay").innerHTML = new Date().toDateString();
 
-checkHour = new Date().getHours();
-
-console.log(checkHour);
+checkHour = new Date().getHours() -8;
 
 
 for(let i = 9; i <= 17; i++){
@@ -17,3 +15,22 @@ for(let i = 9; i <= 17; i++){
     }
     document.getElementById("hour" + i).classList.remove("hour");
 };
+
+document.getElementById("saveBtn9").onclick = function() {saveText(9)};
+document.getElementById("saveBtn10").onclick = function() {saveText(10)};
+document.getElementById("saveBtn11").onclick = function() {saveText(11)};
+document.getElementById("saveBtn12").onclick = function() {saveText(12)};
+document.getElementById("saveBtn13").onclick = function() {saveText(13)};
+document.getElementById("saveBtn14").onclick = function() {saveText(14)};
+document.getElementById("saveBtn15").onclick = function() {saveText(15)};
+document.getElementById("saveBtn16").onclick = function() {saveText(16)};
+document.getElementById("saveBtn17").onclick = function() {saveText(17)};
+
+
+
+function saveText(j){
+    currentInput = document.getElementById("textarea"+ j).value;
+    console.log(currentInput);
+    localStorage.setItem("textArea"+ j, currentInput);
+
+}
